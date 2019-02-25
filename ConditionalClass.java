1a16cf4da9676e18bb7c529ddb5ced60ac9fee5d
tulioowmaciel <47994499+tulioowmaciel@users.noreply.github.com>
2019-02-25 20:22:24 -0300

    AULA 4

---
+++ ConditionalClass.java
@@ -0,0 +1,52 @@
+
+public class ConditionalClass {
+
+	public static void main(String[] args) {
+		int age= 116;  
+		
+		// IF( SE ) > o código será executado somente se a condição dentro do IF for verdadeira
+		if (age > 18) { 
+			System.out.println("Você é obrigado a votar!");
+			
+		}
+		
+	if (age > 65) {
+		System.out.println("Você pode optar por votar!");
+		
+	}
+	
+	age =2;
+	if (age < 16) {
+		System.out.println("Você não pode votar!");
+		
+	}
+	
+	if (age <= 16) {
+		System.out.println("Você não pode votar!");
+		
+	}
+	
+	age = 18;
+	if (age >= 18) {
+		System.out.println("Você pode votar!");
+		}
+	
+
+	age = 50;
+	if (age == 50) {
+		System.out.println("você tem 50 anos");
+		
+	}
+	
+	if (age != 100) {
+		System.out.println("Você ainda é novo");
+	}
+	
+	
+	}
+	
+	
+	
+		
+	
+}
